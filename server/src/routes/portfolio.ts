36af4b691a9ef6b06dcacb238e54a347499a48d5
GP <exchequer598@gmail.com>
2019-06-12 18:38:29 +0530

    backend/routes: /portfolio route.

---
+++ server/src/routes/portfolio.ts
@@ -0,0 +1,71 @@
+import { IRouterContext } from 'koa-tree-router';
+import Database from '../utils/database';
+
+// Initializers.
+const db = Database.getInstance();
+const TABLE = 'portfolios';
+
+/**
+ * Portfolio routes.
+ */
+
+/**
+ * Portfolio select route.
+ * Responds with the results of a SELECT query on the database.
+ *
+ * @export
+ * @param {IRouterContext} Koa context.
+ * @param { () => Promise<any>} Koa next.
+ */
+export async function portfolioSelectRoute(ctx: IRouterContext, next: () => Promise<any>) {
+    const username = ctx.params.username;
+    const results = db.select(`SELECT * FROM ${TABLE} WHERE username = '${username}'`);
+    ctx.body = results;
+    await next();
+}
+
+/**
+ * Portfolio insert route.
+ * Responds with the results of an INSERT query on the database.
+ *
+ * @export
+ * @param {IRouterContext} Koa context.
+ * @param { () => Promise<any>} Koa next.
+ */
+export async function portfolioInsertRoute(ctx: IRouterContext, next: () => Promise<any>) {
+    const username = ctx.params.username;
+    const payload = ctx.request['body'];
+    db.insert(TABLE, {
+        username,
+        ...payload,
+    });
+    ctx.body = {
+        username,
+        operation: 'INSERT',
+        success: true,
+        inserted: payload,
+    };
+    await next();
+}
+
+/**
+ * Portfolio update route.
+ * Responds with the results of an UPDATE query on the database.
+ *
+ * @export
+ * @param {IRouterContext} Koa context.
+ * @param { () => Promise<any>} Koa next.
+ */
+export async function portfolioUpdateRoute(ctx: IRouterContext, next: () => Promise<any>) {
+    const username = ctx.params.username;
+    const payload = ctx.request['body'];
+    const { id, ...updates } = payload;
+    db.update(TABLE, updates, { id, username });
+    ctx.body = {
+        username,
+        operation: 'UPDATE',
+        success: true,
+        updated: payload,
+    };
+    await next();
+}
